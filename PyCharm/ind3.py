ea3a6b19251e9df628398dea6f16a73f104d92bd
nbobrov8 <bobrovn8@yandex.ru>
2021-05-02 16:50:15 +0300

    add ind 3

--- PyCharm/ind3.py
+++ PyCharm/ind3.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 if __name__ == '__main__':
     d = int(input('Введите день: '))
     m = int(input('Введите месяц: '))
     y = int(input('Введите год: '))
+    x = 0
 
-    cc = y % 4 == 0 and y % 100 != 0 or y % 400 == 0
-    bb = [31, 28 + int(cc), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    bb = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    print(sum(bb[:m - 1]) + d)
+    for i in range(m-1):
+        x += bb[i]
+
+    x += d
+    
+    if y % 4 == 0 and y % 100 != 0 or y % 400 == 0:
+        x += 1
+
+    print(x)
+    
